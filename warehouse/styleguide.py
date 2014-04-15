e62cd10b803f030f0a6e1120b874792dd316cc07
Brian Wickman <wickman@twitter.com>
2014-04-15 13:26:01 -0400

    Add tests.

--- warehouse/styleguide.py
+++ warehouse/styleguide.py
@@ -2,17 +2,14 @@ import ast
 
 
 class FlaskSessionCheck(object):
-    _ERROR_TEXT = 'E498 Using Flask sessions is not allowed!'
+    CODE = 'E498'
+    ERROR_TEXT = 'Using Flask sessions is not allowed!'
     name = 'FlaskSessionCheck'
     version = '0.1.0'
 
     @classmethod
-    def import_from(cls, node):
-        if isinstance(node, ast.Import):
-            return [alias.name for alias in node.names]
-        elif isinstance(node, ast.ImportFrom):
-            return [node.module]
-        return []
+    def error_text(cls):
+        return '{} {}'.format(cls.CODE, cls.ERROR_TEXT)
 
     def __init__(self, tree, filename):
         self.tree = tree
@@ -21,11 +18,11 @@ class FlaskSessionCheck(object):
     def run(self):
         # PEP8 expects yields of (lineno, offset, text, <ignored>)
         def error_from_leaf(leaf):
-            return (leaf.lineno, leaf.col_offset, self._ERROR_TEXT, None)
+            return (leaf.lineno, leaf.col_offset, self.error_text(), None)
 
         for leaf in self.tree.body:
             if not isinstance(leaf, (ast.Import, ast.ImportFrom)):
-              continue
+                continue
 
             import_names = [alias.name for alias in leaf.names]
 
